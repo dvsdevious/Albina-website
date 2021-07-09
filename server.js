cadc341a9b793b93c34e7fdbb74adf9e31f20da5
dvsdevious <daria.sheremeteva@gmail.com>
2021-07-09 21:55:31 +0200

    passport authentification

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const flash = require("express-session");
+
+const initializePassport = require("./passport-config");
+initializePassport(passport, (email) =>
+  users.find((user) => user.email === email)
+);
+
 const users = [];
 
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { name: "Kyle" });
@@ -14,7 +37,14 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
@@ -32,7 +62,6 @@ app.post("/register", async (req, res) => {
   } catch {
     res.redirect("/register");
   }
-  console.log(users);
 });
 
 app.listen(3000);
