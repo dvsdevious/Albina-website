992357d65a1e9d4c1bad985e4ecdef99549f8bfd
dvsdevious <daria.sheremeteva@gmail.com>
2021-07-12 18:25:28 +0200

    create a new php file for a database

--- check.php
+++ check.php
@@ -1,4 +1,30 @@
 <?php
+$name = filter_var(trim($_POST['name']),
+FILTER_SANITIZE_STRING);
 $login = filter_var(trim($_POST['login']),
 FILTER_SANITIZE_STRING);
+$password = filter_var(trim($_POST['password']),
+FILTER_SANITIZE_STRING);
+
+if(mb_strlen($name) < 5 || mb_strlen($name)>50) {
+    echo"Недопустимая длина";
+    exit();
+    } else if(mb_strlen($login) < 5 || mb_strlen($login) > 90) {
+        echo "Недопустимая длина";
+        exit();
+    } else if(mb_strlen($password) < 5 || mb_strlen($password) > 6) {
+        echo "Недопустимая длина";
+        exit();
+    }
+
+    $pass = md5($pass,"dvsbmbls@123");
+
+    $dbh=mysqli_connect ("localhost", "cpUsername_dbUsername", "password")
+or die ('I cannot connect to the database.');
+mysql_select_db ("cpUsername_dbName");
+    $mysql->query("INSERT INTO `users`(`login`, `name`, `password`) VALUES('$name','$login','$password')");
+
+    $mysql->close();
+
+    header('Location: /');
 ?>
