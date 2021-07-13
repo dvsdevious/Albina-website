97033736d3102dd9c026847db0acb54afddee399
dvsdevious <daria.sheremeteva@gmail.com>
2021-07-13 19:21:07 +0200

    created functions php

--- Includes/signup.inc.php
+++ Includes/signup.inc.php
@@ -1,9 +1,39 @@
 <?php 
 
 if (isset($_POST['submit'])) {
-    echo "it works";
+
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $username=$_POST['uid'];
+    $pwd=$_POST['pwd'];
+    $pwdRepeat=$_POST['pwdrepeat'];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputsSignup($name, $email, $username, $pwd, $pwdRepeat) !== false) {
+    header("location: ../signup.html?error=emptyinput");
+    exit();
+    }
+    if (invalidUid($username) !== false) {
+    header("location: ../signup.html?error=invaliduid");
+    exit();
+    }
+    if (invalidEmail($email) !== false) {
+    header("location: ../signup.html?error=invaliduid");
+    exit();
+    }
+    if (pwdMatch($pwd, $pwdRepeat) !== false) {
+    header("location: ../signup.html?error=passwordsdontmatch");
+    exit();
+    }
+    if (uidExists($conn, $username) !== false) {
+    header("location: ../signup.html?error=usernametaken");
+    exit();
+    }
 }
 else {
-    header("location:../signup.html");
+    header("location: ../signup.html");
+    exit();
 }
 ?>
