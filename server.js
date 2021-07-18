66ee6b8753dd042c9d44214558b358ec1cf6c9fd
dvsdevious <daria.sheremeteva@gmail.com>
2021-07-18 21:25:43 +0200

    update the login and register pages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const port = 80;
 
 // Import routes
 const authRoute = require("./routes/auth");
@@ -26,4 +27,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/user", authRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(3000, () => console.log("Server Up and running"));
+app.listen(port, function (error) {
+  if (error) {
+    console.log("Something went wrong", error);
+  } else {
+    console.log("Server is listening on port" + port);
+  }
+});
